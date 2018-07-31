a7a2436f01af7fb5a1ad34b7e76d4405aa500207
Mona Sarmadi <mona.sarmadi@yahoo.com>
2018-07-31 14:31:17 +0430

    Add plugin and create html report format

--- src/test/java/com/f_secure/Runner.java
+++ src/test/java/com/f_secure/Runner.java
@@ -5,7 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = {"classpath:features"},
+@CucumberOptions(features = {"classpath:features"}, format = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
                 glue = {"com.f_secure.features"})
 public class Runner {
 }
